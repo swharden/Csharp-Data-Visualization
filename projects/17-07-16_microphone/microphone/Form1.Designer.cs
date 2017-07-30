4f765b8846d6469475dfc11f8b77fde0d0bb080f
Scott W Harden <swharden@gmail.com>
2017-07-30 16:11:59 -0400

    realtime audio FFT graphing

---
+++ projects/17-07-16_microphone/microphone/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace microphone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scottPlotUC1 = new ScottPlot.ScottPlotUC();
+            this.scottPlotUC2 = new ScottPlot.ScottPlotUC();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // scottPlotUC1
+            // 
+            this.scottPlotUC1.Location = new System.Drawing.Point(12, 12);
+            this.scottPlotUC1.Name = "scottPlotUC1";
+            this.scottPlotUC1.Size = new System.Drawing.Size(1108, 402);
+            this.scottPlotUC1.TabIndex = 4;
+            // 
+            // scottPlotUC2
+            // 
+            this.scottPlotUC2.Location = new System.Drawing.Point(12, 420);
+            this.scottPlotUC2.Name = "scottPlotUC2";
+            this.scottPlotUC2.Size = new System.Drawing.Size(1108, 384);
+            this.scottPlotUC2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1021, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 816);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.scottPlotUC2);
+            this.Controls.Add(this.scottPlotUC1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private ScottPlot.ScottPlotUC scottPlotUC1;
+        private ScottPlot.ScottPlotUC scottPlotUC2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ projects/17-07-16_microphone/microphone/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NAudio.Wave; // installed with nuget
+using NAudio.CoreAudioApi;
+using System.Numerics;
+
+
+namespace microphone
+{
+    public partial class Form1 : Form
+    {
+
+        public WaveIn wi;
+        public BufferedWaveProvider bwp;
+        public Int32 envelopeMax;
+
+        private int RATE = 44100; // sample rate of the sound card
+        private int BUFFERSIZE = (int) Math.Pow(2,13); // must be a multiple of 2
+        //private int BUFFERSIZE = 2048; // must be a multiple of 2
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // see what audio devices are available
+            int devcount = WaveIn.DeviceCount;
+            Console.Out.WriteLine("Device Count: {0}.", devcount);
+
+            // get the WaveIn class started
+            WaveIn wi = new WaveIn();
+            wi.DeviceNumber = 0;
+            wi.WaveFormat = new NAudio.Wave.WaveFormat(RATE, 1);
+
+            // create a wave buffer and start the recording
+            wi.DataAvailable += new EventHandler<WaveInEventArgs>(wi_DataAvailable);
+            bwp = new BufferedWaveProvider(wi.WaveFormat);
+            bwp.BufferLength = BUFFERSIZE * 2;
+
+            bwp.DiscardOnBufferOverflow = true;
+            wi.StartRecording();
+
+        }
+
+        // adds data to the audio recording buffer
+        void wi_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            bwp.AddSamples(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        public void UpdateAudioGraph()
+        {
+            // read the bytes from the stream
+            int frameSize = BUFFERSIZE;
+            var frames = new byte[frameSize];
+            bwp.Read(frames, 0, frameSize);
+            if (frames.Length == 0) return;
+            if (frames[frameSize-2] == 0) return;
+            
+            timer1.Enabled = false;
+
+            // convert it to int32 manually (and a double for scottplot)
+            int SAMPLE_RESOLUTION = 16;
+            int BYTES_PER_POINT = SAMPLE_RESOLUTION / 8;
+            Int32[] vals = new Int32[frames.Length/BYTES_PER_POINT];
+            double[] Ys = new double[frames.Length / BYTES_PER_POINT];
+            double[] Xs = new double[frames.Length / BYTES_PER_POINT];
+            double[] Ys2 = new double[frames.Length / BYTES_PER_POINT];
+            double[] Xs2 = new double[frames.Length / BYTES_PER_POINT];
+            for (int i=0; i<vals.Length; i++)
+            {
+                // bit shift the byte buffer into the right variable format
+                byte hByte = frames[i * 2 + 1];
+                byte lByte = frames[i * 2 + 0];
+                vals[i] = (int)(short)((hByte << 8) | lByte);
+                Xs[i] = i;
+                Ys[i] = vals[i];
+                Xs2[i] = (double)i/Ys.Length*RATE/1000.0; // units are in kHz
+            }
+
+            // update scottplot (PCM, time domain)
+            scottPlotUC1.Xs = Xs;
+            scottPlotUC1.Ys = Ys;
+
+            //update scottplot (FFT, frequency domain)
+            Ys2 = FFT(Ys);
+            scottPlotUC2.Xs = Xs2.Take(Xs2.Length / 2).ToArray();
+            scottPlotUC2.Ys = Ys2.Take(Ys2.Length / 2).ToArray();
+            
+
+            // update the displays
+            scottPlotUC1.UpdateGraph();
+            scottPlotUC2.UpdateGraph();
+
+            Application.DoEvents();
+            scottPlotUC1.Update();
+            scottPlotUC2.Update();
+            
+            timer1.Enabled = true;
+
+        }
+
+        public double[] FFT(double[] data)
+        {
+            double[] fft = new double[data.Length]; // this is where we will store the output (fft)
+            Complex[] fftComplex = new Complex[data.Length]; // the FFT function requires complex format
+            for (int i = 0; i < data.Length; i++)
+            {
+                fftComplex[i] = new Complex(data[i], 0.0); // make it complex format (imaginary = 0)
+            }
+            Accord.Math.FourierTransform.FFT(fftComplex, Accord.Math.FourierTransform.Direction.Forward);
+            for (int i = 0; i < data.Length; i++)
+            {
+                fft[i] = fftComplex[i].Magnitude; // back to double
+                //fft[i] = Math.Log10(fft[i]); // convert to dB
+            }
+            return fft;
+            //todo: this could be much faster by reusing variables
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateAudioGraph();
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateAudioGraph();
+        }
+    }
+}
