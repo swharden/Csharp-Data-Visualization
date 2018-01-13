5d007abdb5b857b28dd0b2108c26d8bde63c3436
Scott W Harden <swharden@gmail.com>
2018-01-13 15:47:58 -0500

    bitmap scroll/roll example

---
+++ projects/18-01-10_fast_pixel_bitmap/WindowsFormsApp1/02 bitmap scroll/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace _02_bitmap_scroll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pictureBox1.Location = new System.Drawing.Point(20, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "http://www.GitHub.com/SWHarden/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "management of continuously updating 2d data";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "C# Data Visualization Examples";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 525);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ projects/18-01-10_fast_pixel_bitmap/WindowsFormsApp1/02 bitmap scroll/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Imaging; // for ImageLockMode
+using System.Runtime.InteropServices; // for Marshal
+
+namespace _02_bitmap_scroll
+{
+    public partial class Form1 : Form
+    {
+        private static int update_count = 0;
+        private static Random rand = new Random();
+
+        private static List<List<double>> data; // a list of column pixel values
+
+        public Form1()
+        {
+            InitializeComponent();
+            Data_init();
+        }
+
+        /// <summary>
+        /// return a list of random doubles from 0.0 to 1.0
+        /// </summary>
+        private List<double> Generate_list_random(int itemCount) {
+            List<double> list = new List<double>(itemCount);
+            for (int i=0; i< itemCount; i++)
+            {
+                list.Add(rand.NextDouble());
+            }
+            return list;
+        }
+        
+
+        /// <summary>
+        /// fill "data" with 2d random data
+        /// </summary>
+        private void Data_init()
+        {
+            int data_samples = pictureBox1.Width;
+            int data_size = pictureBox1.Height;
+
+            data = new List<List<double>>();
+            for (int i=0; i<data_samples; i++)
+            {
+                data.Add(Generate_list_random(data_size));
+            }
+
+            System.Console.WriteLine("generated random data");
+
+        }
+
+        private void Bitmap_from_data()
+        {
+            // create a bitmap we will work with
+            Bitmap bitmap = new Bitmap(data.Count, data[0].Count, PixelFormat.Format8bppIndexed);
+
+            // modify the indexed palette to make it grayscale
+            ColorPalette pal = bitmap.Palette;
+            for (int i = 0; i < 256; i++)
+                pal.Entries[i] = Color.FromArgb(255, i, i, i);
+            bitmap.Palette = pal;
+
+            // prepare to access data via the bitmapdata object
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                                                    ImageLockMode.ReadOnly, bitmap.PixelFormat);
+
+            // create a byte array to reflect each pixel in the image
+            byte[] pixels = new byte[bitmapData.Stride * bitmap.Height];
+
+            // fill pixel array with data
+            for (int col=0; col<data.Count; col++)
+            {
+                for (int row=0; row<data[col].Count; row++)
+                {
+                    int bytePosition = row * bitmapData.Stride + col;
+                    pixels[bytePosition] = (byte)(255 * data[col][row]);
+                }
+            }
+
+            // turn the byte array back into a bitmap
+            Marshal.Copy(pixels, 0, bitmapData.Scan0, pixels.Length);
+            bitmap.UnlockBits(bitmapData);
+
+            // apply the bitmap to the picturebox
+            pictureBox1.Image = bitmap;
+
+        }
+
+        private void Bitmap_roll()
+        {
+            data.Insert(data.Count, data[0]);
+            data.RemoveAt(0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i=0; i<5; i++)
+            {
+                Bitmap_roll();
+            }
+            
+            Bitmap_from_data();
+            update_count += 1;
+            label4.Text = string.Format("Update count: {0}", update_count);
+        }
+    }
+}
