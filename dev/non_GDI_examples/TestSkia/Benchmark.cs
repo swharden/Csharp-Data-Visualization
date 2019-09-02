62b2e4558edf741a355ac5bcbf692bb7b9c1faa0
Scott W Harden <swharden@gmail.com>
2019-09-01 21:38:23 -0400

    add non-GDI examples

---
+++ dev/non_GDI_examples/TestSkia/Benchmark.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSkia
+{
+    public class Benchmark : IDisposable
+    {
+        System.Diagnostics.Stopwatch stopwatch;
+        bool silent;
+
+        public Benchmark(bool silent = false)
+        {
+            stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            this.silent = silent;
+        }
+
+        public double elapsedMilliseconds
+        {
+            get
+            {
+                return stopwatch.ElapsedTicks * 1000.0 / System.Diagnostics.Stopwatch.Frequency;
+            }
+        }
+
+        public double hertz
+        {
+            get
+            {
+                return 1000.0 / elapsedMilliseconds;
+            }
+        }
+
+        public string GetMessage(string description = "completed")
+        {
+            stopwatch.Stop();
+            return string.Format(description + " in {0:0.00} ms {1:0.00 Hz}", elapsedMilliseconds, hertz);
+        }
+
+        public void Dispose()
+        {
+            stopwatch.Stop();
+            if (!silent)
+                Console.WriteLine(GetMessage());
+        }
+    }
+}
---
+++ dev/non_GDI_examples/TestSkia/Form1.cs
@@ -0,0 +1,75 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestSkia
+{
+    public partial class Form1 : Form
+    {
+        private SKSurface surface;
+        private bool rendering = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateSurfaceFromCurrentDimensions();
+        }
+
+        private void PictureBox1_Resize(object sender, EventArgs e)
+        {
+            UpdateSurfaceFromCurrentDimensions();
+        }
+
+        private void UpdateSurfaceFromCurrentDimensions()
+        {
+            SKImageInfo info = new SKImageInfo(pictureBox1.Width, pictureBox1.Height);
+            surface = SKSurface.Create(info);
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (rendering)
+                return;
+            else
+                rendering = true;
+
+            var benchmark = new Benchmark();
+
+            if (rbText.Checked)
+                pictureBox1.Image = TestImages.Text(surface, (int)nudCount.Value);
+            else if (rbLines.Checked)
+                pictureBox1.Image = TestImages.Lines(surface, (int)nudCount.Value);
+            else
+                throw new NotImplementedException();
+
+            lblStatus.Text = benchmark.GetMessage();
+            rendering = false;
+        }
+
+        private void BtnStartStop_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled = false;
+                btnStartStop.Text = "Start";
+            }
+            else
+            {
+                timer1.Enabled = true;
+                btnStartStop.Text = "Stop";
+            }
+        }
+    }
+}
