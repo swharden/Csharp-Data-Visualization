0f96d6c4fc188679ba8b9f11fdc79d7e49887072
Scott W Harden <swharden@gmail.com>
2019-09-08 18:03:26 -0400

    SkiaSharp OpenGL demo

---
+++ examples/2019-09-08-SkiaSharp-openGL/SkiaDemo/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SkiaDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBenchmark = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.glControl1 = new OpenTK.GLControl();
+            this.SuspendLayout();
+            // 
+            // btnBenchmark
+            // 
+            this.btnBenchmark.Location = new System.Drawing.Point(12, 12);
+            this.btnBenchmark.Name = "btnBenchmark";
+            this.btnBenchmark.Size = new System.Drawing.Size(75, 23);
+            this.btnBenchmark.TabIndex = 1;
+            this.btnBenchmark.Text = "benchmark";
+            this.btnBenchmark.UseVisualStyleBackColor = true;
+            this.btnBenchmark.Click += new System.EventHandler(this.BtnBenchmark_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(199, 17);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(57, 13);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "not yet run";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(93, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // glControl1
+            // 
+            this.glControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(12, 41);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(776, 397);
+            this.glControl1.TabIndex = 4;
+            this.glControl1.VSync = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.glControl1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnBenchmark);
+            this.Name = "Form1";
+            this.Text = "SkiaSharp Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBenchmark;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private OpenTK.GLControl glControl1;
+    }
+}
+
---
+++ examples/2019-09-08-SkiaSharp-openGL/SkiaDemo/Form1.cs
@@ -0,0 +1,100 @@
+﻿using OpenTK.Graphics.ES20;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkiaDemo
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        SKColorType colorType = SKColorType.Rgba8888;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            glControl1.VSync = false; // why?
+            glControl1.Paint += new PaintEventHandler(RenderWithOpenGL);
+        }
+
+        private void RenderWithOpenGL(object sender, PaintEventArgs e)
+        {
+            Control sctl = (Control)sender;
+            int width = sctl.Width;
+            int height = sctl.Height;
+
+            // setup the Skia surface using OpenGL
+
+            GRContext contextOpenGL = GRContext.Create(GRBackend.OpenGL, GRGlInterface.CreateNativeGlInterface());
+
+            GL.GetInteger(GetPName.FramebufferBinding, out var framebuffer);
+            GRGlFramebufferInfo glInfo = new GRGlFramebufferInfo((uint)framebuffer, colorType.ToGlSizedFormat());
+            GL.GetInteger(GetPName.StencilBits, out var stencil);
+            GRBackendRenderTarget renderTarget = new GRBackendRenderTarget(width, height, contextOpenGL.GetMaxSurfaceSampleCount(colorType), stencil, glInfo);
+
+            SKSurface surface = SKSurface.Create(contextOpenGL, renderTarget, GRSurfaceOrigin.BottomLeft, colorType);
+
+            // Draw some stuff on the canvas
+
+            surface.Canvas.Clear(SKColor.Parse("#FFFFFF")); // adds about 3ms fullscreen
+
+            byte alpha = 128;
+            var paint = new SKPaint();
+            for (int i = 0; i < 1_000; i++)
+            {
+                float x1 = (float)(rand.NextDouble() * width);
+                float x2 = (float)(rand.NextDouble() * width);
+                float y1 = (float)(rand.NextDouble() * height);
+                float y2 = (float)(rand.NextDouble() * height);
+
+                paint.Color = new SKColor(
+                        red: (byte)(rand.NextDouble() * 255),
+                        green: (byte)(rand.NextDouble() * 255),
+                        blue: (byte)(rand.NextDouble() * 255),
+                        alpha: alpha
+                    );
+
+                surface.Canvas.DrawLine(x1, y1, x2, y2, paint);
+            }
+
+            surface.Canvas.Flush();
+            glControl1.SwapBuffers();
+
+            // prevent memory access violations by disposing before exiting
+            renderTarget?.Dispose();
+            contextOpenGL?.Dispose();
+            surface?.Dispose(); 
+        }
+
+        private void BtnBenchmark_Click(object sender, EventArgs e)
+        {
+            int count = 250;
+
+            btnBenchmark.Enabled = false;
+            System.Diagnostics.Stopwatch stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+            progressBar1.Maximum = count;
+            for (int i = 0; i < count; i++)
+            {
+                lblStatus.Text = $"on render {i + 1} of {count}...";
+                progressBar1.Value = i + 1;
+                progressBar1.Value = i;
+                Application.DoEvents(); // adds about 1ms fullscreen
+                glControl1.Refresh();
+            }
+            progressBar1.Value = 0;
+
+            double meanSec = (double)stopwatch.ElapsedTicks / System.Diagnostics.Stopwatch.Frequency / count;
+            lblStatus.Text = string.Format("mean render {0:0.00} ms ({1:0.00} Hz)", meanSec * 1000, 1.0 / meanSec);
+            btnBenchmark.Enabled = true;
+        }
+    }
+}
