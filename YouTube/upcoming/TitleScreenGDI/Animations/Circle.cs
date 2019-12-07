03c2cb4fcf40bdf56d104c993949f706ab462497
Scott W Harden <swharden@gmail.com>
2019-12-07 10:01:32 -0500

    start youtube title screen

---
+++ YouTube/upcoming/TitleScreenGDI/Animations/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TitleScreenGDI.Animations
+{
+    public class Circle
+    {
+        public float X, Y;
+        public double velX, velY;
+        public PointF LocationF { get { return new PointF(X, Y); } }
+        public Point Location { get { return Point.Round(LocationF); } }
+    }
+}
---
+++ YouTube/upcoming/TitleScreenGDI/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TitleScreenGDI
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnRedraw_Click(null, null);
+        }
+
+        Animations.CirclesAndLines stuff;
+        Bitmap bmp;
+
+        private void btnRedraw_Click(object sender, EventArgs e)
+        {
+            stuff = new Animations.CirclesAndLines(pictureBox1.Width, pictureBox1.Height, 10);
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = Animations.RenderGDI.Render(stuff, bmp);
+        }
+
+        private void btnIterate_Click(object sender, EventArgs e)
+        {
+            stuff.Iterate(5);
+            pictureBox1.Image = Animations.RenderGDI.Render(stuff, bmp);
+        }
+
+        private void cbRun_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = cbRun.Checked;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            btnIterate_Click(null, null);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            btnRedraw_Click(null, null);
+        }
+    }
+}
