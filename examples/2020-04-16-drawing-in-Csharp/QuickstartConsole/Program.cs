8fb9a92fc997e8e94fbbb5329b336b6faf19b3ad
Scott W Harden <swharden@gmail.com>
2020-04-16 23:14:24 -0400

    drawing in Csharp example

---
+++ examples/2020-04-16-drawing-in-Csharp/QuickstartConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace QuickstartConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            using (var brush = new SolidBrush(Color.White))
+            using (var pen = new Pen(brush))
+            using (var bmp = new Bitmap(600, 400))
+            using (var gfx = Graphics.FromImage(bmp))
+            {
+                gfx.SmoothingMode = SmoothingMode.AntiAlias;
+
+                gfx.Clear(Color.Navy);
+                for (int i = 0; i < 1000; i++)
+                {
+                    gfx.DrawLine(pen: pen,
+                        x1: rand.Next(bmp.Width),
+                        y1: rand.Next(bmp.Height),
+                        x2: rand.Next(bmp.Width),
+                        y2: rand.Next(bmp.Height));
+                }
+
+                string saveFilePath = System.IO.Path.GetFullPath("console.png");
+                bmp.Save(saveFilePath, ImageFormat.Png);
+                Console.WriteLine($"Saved: {saveFilePath}");
+            }
+
+            Console.WriteLine("press ENTER to exit...");
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ examples/2020-04-16-drawing-in-Csharp/QuickstartWinForms/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickstartWinForms
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        Bitmap bmp;
+        Graphics gfx;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Render();
+        }
+
+        private void pictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            Render();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Render();
+        }
+
+        void ResizeBitmap()
+        {
+            bmp?.Dispose();
+            gfx?.Dispose();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gfx = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+        }
+
+        void Render()
+        {
+            if (bmp is null || bmp?.Size != pictureBox1.Size)
+                ResizeBitmap();
+
+            using (var brush = new SolidBrush(Color.White))
+            using (var pen = new Pen(brush))
+            {
+                gfx.SmoothingMode = SmoothingMode.AntiAlias;
+
+                gfx.Clear(Color.Navy);
+                for (int i = 0; i < 1000; i++)
+                {
+                    gfx.DrawLine(pen: pen,
+                        x1: rand.Next(bmp.Width),
+                        y1: rand.Next(bmp.Height),
+                        x2: rand.Next(bmp.Width),
+                        y2: rand.Next(bmp.Height));
+                }
+            }
+
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
