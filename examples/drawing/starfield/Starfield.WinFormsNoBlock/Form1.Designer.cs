a304b092f4f09cd5fc3d2ceb55268ac9eec128cd
Scott W Harden <swharden@gmail.com>
2020-04-26 16:08:53 -0400

    starfield noblock

---
+++ examples/drawing/starfield/Starfield.WinFormsNoBlock/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Starfield.WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.rb500 = new System.Windows.Forms.RadioButton();
+            this.rb100k = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Purple;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(560, 286);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(101, 12);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(397, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(484, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 48);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "100%";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // rb500
+            // 
+            this.rb500.AutoSize = true;
+            this.rb500.Checked = true;
+            this.rb500.Location = new System.Drawing.Point(12, 12);
+            this.rb500.Name = "rb500";
+            this.rb500.Size = new System.Drawing.Size(68, 17);
+            this.rb500.TabIndex = 3;
+            this.rb500.TabStop = true;
+            this.rb500.Text = "500 stars";
+            this.rb500.UseVisualStyleBackColor = true;
+            this.rb500.CheckedChanged += new System.EventHandler(this.rb500_CheckedChanged);
+            // 
+            // rb100k
+            // 
+            this.rb100k.AutoSize = true;
+            this.rb100k.Location = new System.Drawing.Point(12, 32);
+            this.rb100k.Name = "rb100k";
+            this.rb100k.Size = new System.Drawing.Size(89, 17);
+            this.rb100k.TabIndex = 4;
+            this.rb100k.Text = "100,000 stars";
+            this.rb100k.UseVisualStyleBackColor = true;
+            this.rb100k.CheckedChanged += new System.EventHandler(this.rb100k_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(504, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Alpha";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rb100k);
+            this.Controls.Add(this.rb500);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Starfield in Windows Forms (non-blocking)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton rb500;
+        private System.Windows.Forms.RadioButton rb100k;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ examples/drawing/starfield/Starfield.WinFormsNoBlock/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Starfield.WinForms
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bmpLive = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bmpLast = (Bitmap)bmpLive.Clone();
+
+            var renderThread = new Thread(new ThreadStart(RenderForever));
+            renderThread.Start();
+        }
+
+        static readonly Field field = new Field(500);
+        static Bitmap bmpLive;
+        static Bitmap bmpLast;
+        private static void RenderForever()
+        {
+            double maxFPS = 100;
+            double minFramePeriodMsec = 1000.0 / maxFPS;
+
+            Stopwatch stopwatch = new Stopwatch();
+            while (true)
+            {
+                stopwatch.Restart();
+                field.Advance();
+                field.Render(bmpLive, starColor);
+                lock (bmpLast)
+                {
+                    bmpLast.Dispose();
+                    bmpLast = (Bitmap)bmpLive.Clone();
+                }
+
+                double msToWait = minFramePeriodMsec - stopwatch.ElapsedMilliseconds;
+                if (msToWait > 0)
+                    Thread.Sleep((int)msToWait);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lock (bmpLast)
+            {
+                pictureBox1.Image?.Dispose();
+                pictureBox1.Image = (Bitmap)bmpLast.Clone();
+            }
+        }
+
+        private void rb500_CheckedChanged(object sender, EventArgs e)
+        {
+            field.Reset(500);
+        }
+
+        private void rb100k_CheckedChanged(object sender, EventArgs e)
+        {
+            field.Reset(100_000);
+        }
+
+        static Color starColor = Color.White;
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = $"{trackBar1.Value}%";
+            byte alpha = (byte)(trackBar1.Value * 255 / 100);
+            starColor = Color.FromArgb(alpha, Color.White);
+        }
+    }
+}
