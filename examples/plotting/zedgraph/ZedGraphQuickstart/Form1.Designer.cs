c0abce15e9040a981fab6315de3b4a4deab0f239
Scott W Harden <swharden@gmail.com>
2020-05-16 00:21:00 -0400

    zedgraph quickstart

---
+++ examples/plotting/zedgraph/ZedGraphQuickstart/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ZedGraphQuickstart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.BarButton = new System.Windows.Forms.Button();
+            this.ScatterButton = new System.Windows.Forms.Button();
+            this.LineButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 41);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(560, 327);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // BarButton
+            // 
+            this.BarButton.Location = new System.Drawing.Point(12, 12);
+            this.BarButton.Name = "BarButton";
+            this.BarButton.Size = new System.Drawing.Size(75, 23);
+            this.BarButton.TabIndex = 1;
+            this.BarButton.Text = "Bar";
+            this.BarButton.UseVisualStyleBackColor = true;
+            this.BarButton.Click += new System.EventHandler(this.BarButton_Click);
+            // 
+            // ScatterButton
+            // 
+            this.ScatterButton.Location = new System.Drawing.Point(93, 12);
+            this.ScatterButton.Name = "ScatterButton";
+            this.ScatterButton.Size = new System.Drawing.Size(75, 23);
+            this.ScatterButton.TabIndex = 2;
+            this.ScatterButton.Text = "Scatter";
+            this.ScatterButton.UseVisualStyleBackColor = true;
+            this.ScatterButton.Click += new System.EventHandler(this.ScatterButton_Click);
+            // 
+            // LineButton
+            // 
+            this.LineButton.Location = new System.Drawing.Point(174, 12);
+            this.LineButton.Name = "LineButton";
+            this.LineButton.Size = new System.Drawing.Size(75, 23);
+            this.LineButton.TabIndex = 3;
+            this.LineButton.Text = "Line";
+            this.LineButton.UseVisualStyleBackColor = true;
+            this.LineButton.Click += new System.EventHandler(this.LineButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 380);
+            this.Controls.Add(this.LineButton);
+            this.Controls.Add(this.ScatterButton);
+            this.Controls.Add(this.BarButton);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "ZedGraph Quickstart";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button BarButton;
+        private System.Windows.Forms.Button ScatterButton;
+        private System.Windows.Forms.Button LineButton;
+    }
+}
+
---
+++ examples/plotting/zedgraph/ZedGraphQuickstart/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZedGraphQuickstart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ScatterButton_Click(null, null);
+        }
+
+        private Random rand = new Random(0);
+        private double[] RandomWalk(int points, double start = 100, double mult = 50)
+        {
+            // return an array of difting random numbers
+            double[] values = new double[points];
+            values[0] = start;
+            for (int i = 1; i < points; i++)
+                values[i] = values[i - 1] + (rand.NextDouble() - .5) * mult;
+            return values;
+        }
+
+        private double[] Consecutive(int points)
+        {
+            // return an array of ascending numbers starting at 1
+            double[] values = new double[points];
+            for (int i = 0; i < points; i++)
+                values[i] = i;
+            return values;
+        }
+
+        private void BarButton_Click(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 5;
+            double[] xs = Consecutive(pointCount);
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // clear old curves
+            zedGraphControl1.GraphPane.CurveList.Clear();
+
+            // plot the data as bars
+            zedGraphControl1.GraphPane.AddBar("Group A", xs, ys1, Color.Blue);
+            zedGraphControl1.GraphPane.AddBar("Group B", xs, ys2, Color.Red);
+
+            // style the plot
+            zedGraphControl1.GraphPane.Title.Text = $"Bar Plot ({pointCount:n0} points)";
+            zedGraphControl1.GraphPane.XAxis.Title.Text = "Horizontal Axis Label";
+            zedGraphControl1.GraphPane.YAxis.Title.Text = "Vertical Axis Label";
+
+            // auto-axis and update the display
+            zedGraphControl1.GraphPane.XAxis.ResetAutoScale(zedGraphControl1.GraphPane, CreateGraphics());
+            zedGraphControl1.GraphPane.YAxis.ResetAutoScale(zedGraphControl1.GraphPane, CreateGraphics());
+            zedGraphControl1.Refresh();
+        }
+
+        private void ScatterButton_Click(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 100;
+            double[] xs1 = RandomWalk(pointCount);
+            double[] ys1 = RandomWalk(pointCount);
+            double[] xs2 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // clear old curves
+            zedGraphControl1.GraphPane.CurveList.Clear();
+
+            // plot the data as curves
+            var curve1 = zedGraphControl1.GraphPane.AddCurve("Series A", xs1, ys1, Color.Blue);
+            curve1.Line.IsAntiAlias = true;
+
+            var curve2 = zedGraphControl1.GraphPane.AddCurve("Series B", xs2, ys2, Color.Red);
+            curve2.Line.IsAntiAlias = true;
+
+            // style the plot
+            zedGraphControl1.GraphPane.Title.Text = $"Scatter Plot ({pointCount:n0} points)";
+            zedGraphControl1.GraphPane.XAxis.Title.Text = "Horizontal Axis Label";
+            zedGraphControl1.GraphPane.YAxis.Title.Text = "Vertical Axis Label";
+
+            // auto-axis and update the display
+            zedGraphControl1.GraphPane.XAxis.ResetAutoScale(zedGraphControl1.GraphPane, CreateGraphics());
+            zedGraphControl1.GraphPane.YAxis.ResetAutoScale(zedGraphControl1.GraphPane, CreateGraphics());
+            zedGraphControl1.Refresh();
+        }
+
+        private void LineButton_Click(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 100_000;
+            double[] xs = Consecutive(pointCount);
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // clear old curves
+            zedGraphControl1.GraphPane.CurveList.Clear();
+
+            // plot the data as curves
+            var curve1 = zedGraphControl1.GraphPane.AddCurve("Series A", xs, ys1, Color.Blue);
+            curve1.Line.IsAntiAlias = true;
+            curve1.Symbol.IsVisible = false;
+
+            var curve2 = zedGraphControl1.GraphPane.AddCurve("Series B", xs, ys2, Color.Red);
+            curve2.Line.IsAntiAlias = true;
+            curve2.Symbol.IsVisible = false;
+
+            // style the plot
+            zedGraphControl1.GraphPane.Title.Text = $"Scatter Plot ({pointCount:n0} points)";
+            zedGraphControl1.GraphPane.XAxis.Title.Text = "Horizontal Axis Label";
+            zedGraphControl1.GraphPane.YAxis.Title.Text = "Vertical Axis Label";
+
+            // auto-axis and update the display
+            zedGraphControl1.GraphPane.XAxis.ResetAutoScale(zedGraphControl1.GraphPane, CreateGraphics());
+            zedGraphControl1.GraphPane.YAxis.ResetAutoScale(zedGraphControl1.GraphPane, CreateGraphics());
+            zedGraphControl1.Refresh();
+        }
+    }
+}
