0476e349df1e44d64e27c57fb149e2710256715e
Scott W Harden <swharden@gmail.com>
2020-05-15 20:09:30 -0400

    microsoft charting quickstart

---
+++ examples/plotting/microsoft-charting/ChartingQuickstart/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ChartingQuickstart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.BarButton = new System.Windows.Forms.Button();
+            this.ScatterButton = new System.Windows.Forms.Button();
+            this.LineButton = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BarButton
+            // 
+            this.BarButton.Location = new System.Drawing.Point(12, 12);
+            this.BarButton.Name = "BarButton";
+            this.BarButton.Size = new System.Drawing.Size(75, 23);
+            this.BarButton.TabIndex = 1;
+            this.BarButton.Text = "Bar";
+            this.BarButton.UseVisualStyleBackColor = true;
+            this.BarButton.Click += new System.EventHandler(this.BarButton_Click);
+            // 
+            // ScatterButton
+            // 
+            this.ScatterButton.Location = new System.Drawing.Point(93, 12);
+            this.ScatterButton.Name = "ScatterButton";
+            this.ScatterButton.Size = new System.Drawing.Size(75, 23);
+            this.ScatterButton.TabIndex = 2;
+            this.ScatterButton.Text = "Scatter";
+            this.ScatterButton.UseVisualStyleBackColor = true;
+            this.ScatterButton.Click += new System.EventHandler(this.ScatterButton_Click);
+            // 
+            // LineButton
+            // 
+            this.LineButton.Location = new System.Drawing.Point(174, 12);
+            this.LineButton.Name = "LineButton";
+            this.LineButton.Size = new System.Drawing.Size(75, 23);
+            this.LineButton.TabIndex = 3;
+            this.LineButton.Text = "Line";
+            this.LineButton.UseVisualStyleBackColor = true;
+            this.LineButton.Click += new System.EventHandler(this.LineButton_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart1.BorderlineColor = System.Drawing.Color.Black;
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 41);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(560, 313);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 366);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.LineButton);
+            this.Controls.Add(this.ScatterButton);
+            this.Controls.Add(this.BarButton);
+            this.Name = "Form1";
+            this.Text = "Microsoft Charting Quickstart";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BarButton;
+        private System.Windows.Forms.Button ScatterButton;
+        private System.Windows.Forms.Button LineButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ examples/plotting/microsoft-charting/ChartingQuickstart/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ChartingQuickstart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BarButton_Click(null, null);
+        }
+
+        private Random rand = new Random(0);
+        private double[] RandomWalk(int pointCount = 5, double firstValue = 100, double multiplier = 50)
+        {
+            // return an array of difting random numbers
+            double[] values = new double[pointCount];
+            values[0] = firstValue;
+            for (int i = 1; i < pointCount; i++)
+                values[i] = values[i - 1] + (rand.NextDouble() - .5) * multiplier;
+            return values;
+        }
+
+        private double[] Consecutive(int pointCount = 5)
+        {
+            // return an array of consecutive numbers starting at 1
+            double[] values = new double[pointCount];
+            for (int i = 0; i < pointCount; i++)
+                values[i] = i + 1;
+            return values;
+        }
+
+        private void BarButton_Click(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 5;
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // create a series for each line
+            Series series1 = new Series("Group A");
+            series1.Points.DataBindY(ys1);
+            series1.ChartType = SeriesChartType.Column;
+
+            Series series2 = new Series("Group B");
+            series2.Points.DataBindY(ys2);
+            series2.ChartType = SeriesChartType.Column;
+            
+            // add each series to the chart
+            chart1.Series.Clear();
+            chart1.Series.Add(series1);
+            chart1.Series.Add(series2);
+
+            // additional styling
+            chart1.ResetAutoValues();
+            chart1.Titles.Clear();
+            chart1.Titles.Add($"Column Chart ({pointCount} points per series)");
+            chart1.ChartAreas[0].AxisX.Title = "Horizontal Axis Label";
+            chart1.ChartAreas[0].AxisY.Title = "Vertical Axis Label";
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+        }
+
+        private void ScatterButton_Click(object sender, EventArgs e)
+        {
+            // generate some random XY data
+            int pointCount = 1_000;
+            double[] xs1 = RandomWalk(pointCount);
+            double[] ys1 = RandomWalk(pointCount);
+            double[] xs2 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // create a series for each line
+            Series series1 = new Series("Group A");
+            series1.Points.DataBindXY(xs1, ys1);
+            series1.ChartType = SeriesChartType.Line;
+            series1.MarkerStyle = MarkerStyle.Circle;
+
+            Series series2 = new Series("Group B");
+            series2.Points.DataBindXY(xs2, ys2);
+            series2.ChartType = SeriesChartType.Line;
+            series2.MarkerStyle = MarkerStyle.Circle;
+
+            // add each series to the chart
+            chart1.Series.Clear();
+            chart1.Series.Add(series1);
+            chart1.Series.Add(series2);
+
+            // additional styling
+            chart1.ResetAutoValues();
+            chart1.Titles.Clear();
+            chart1.Titles.Add($"Scatter Plot ({pointCount:N0} points per series)");
+            chart1.ChartAreas[0].AxisX.Title = "Horizontal Axis Label";
+            chart1.ChartAreas[0].AxisY.Title = "Vertical Axis Label";
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+        }
+
+        private void LineButton_Click(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 100_000;
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // create a series for each line
+            Series series1 = new Series("Group A");
+            series1.Points.DataBindY(ys1);
+            series1.ChartType = SeriesChartType.FastLine;
+
+            Series series2 = new Series("Group B");
+            series2.Points.DataBindY(ys2);
+            series2.ChartType = SeriesChartType.FastLine;
+
+            // add each series to the chart
+            chart1.Series.Clear();
+            chart1.Series.Add(series1);
+            chart1.Series.Add(series2);
+
+            // additional styling
+            chart1.ResetAutoValues();
+            chart1.Titles.Clear();
+            chart1.Titles.Add($"Fast Line Plot ({pointCount:N0} points per series)");
+            chart1.ChartAreas[0].AxisX.Title = "Horizontal Axis Label";
+            chart1.ChartAreas[0].AxisY.Title = "Vertical Axis Label";
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+        }
+    }
+}
