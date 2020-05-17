c549e0ff81a7df8f5200c08bd150fc09ed8a83ac
Scott W Harden <swharden@gmail.com>
2020-05-17 17:48:31 -0400

    oxyplot quickstart

---
+++ examples/plotting/oxyplot/OxyPlotQuickstart/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace OxyPlotQuickstart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.BarButton = new System.Windows.Forms.Button();
+            this.ScatterButton = new System.Windows.Forms.Button();
+            this.LineButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // plotView1
+            // 
+            this.plotView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plotView1.BackColor = System.Drawing.Color.White;
+            this.plotView1.Location = new System.Drawing.Point(12, 41);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(776, 397);
+            this.plotView1.TabIndex = 0;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // BarButton
+            // 
+            this.BarButton.Location = new System.Drawing.Point(12, 12);
+            this.BarButton.Name = "BarButton";
+            this.BarButton.Size = new System.Drawing.Size(75, 23);
+            this.BarButton.TabIndex = 1;
+            this.BarButton.Text = "Bar";
+            this.BarButton.UseVisualStyleBackColor = true;
+            this.BarButton.Click += new System.EventHandler(this.BarButton_Click);
+            // 
+            // ScatterButton
+            // 
+            this.ScatterButton.Location = new System.Drawing.Point(93, 12);
+            this.ScatterButton.Name = "ScatterButton";
+            this.ScatterButton.Size = new System.Drawing.Size(75, 23);
+            this.ScatterButton.TabIndex = 2;
+            this.ScatterButton.Text = "Scatter";
+            this.ScatterButton.UseVisualStyleBackColor = true;
+            this.ScatterButton.Click += new System.EventHandler(this.ScatterButton_Click);
+            // 
+            // LineButton
+            // 
+            this.LineButton.Location = new System.Drawing.Point(174, 12);
+            this.LineButton.Name = "LineButton";
+            this.LineButton.Size = new System.Drawing.Size(75, 23);
+            this.LineButton.TabIndex = 3;
+            this.LineButton.Text = "Line";
+            this.LineButton.UseVisualStyleBackColor = true;
+            this.LineButton.Click += new System.EventHandler(this.LineButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LineButton);
+            this.Controls.Add(this.ScatterButton);
+            this.Controls.Add(this.BarButton);
+            this.Controls.Add(this.plotView1);
+            this.Name = "Form1";
+            this.Text = "OxyPlot Quickstart";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.Button BarButton;
+        private System.Windows.Forms.Button ScatterButton;
+        private System.Windows.Forms.Button LineButton;
+    }
+}
+
---
+++ examples/plotting/oxyplot/OxyPlotQuickstart/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OxyPlotQuickstart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ScatterButton_Click(null, null);
+        }
+
+        private Random rand = new Random(0);
+        private double[] RandomWalk(int points = 5, double start = 100, double mult = 50)
+        {
+            // return an array of difting random numbers
+            double[] values = new double[points];
+            values[0] = start;
+            for (int i = 1; i < points; i++)
+                values[i] = values[i - 1] + (rand.NextDouble() - .5) * mult;
+            return values;
+        }
+
+        private double[] Consecutive(int pointCount = 5)
+        {
+            // return an array of consecutive numbers starting at 1
+            double[] values = new double[pointCount];
+            for (int i = 0; i < pointCount; i++)
+                values[i] = i + 1;
+            return values;
+        }
+
+        private void BarButton_Click(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 5;
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // create a series of bars and populate them with data
+            var seriesA = new OxyPlot.Series.ColumnSeries()
+            {
+                Title = "Series A",
+                StrokeColor = OxyPlot.OxyColors.Black,
+                FillColor = OxyPlot.OxyColors.Red,
+                StrokeThickness = 1
+            };
+
+            var seriesB = new OxyPlot.Series.ColumnSeries()
+            {
+                Title = "Series B",
+                StrokeColor = OxyPlot.OxyColors.Black,
+                FillColor = OxyPlot.OxyColors.Blue,
+                StrokeThickness = 1
+            };
+
+            for (int i = 0; i < pointCount; i++)
+            {
+                seriesA.Items.Add(new OxyPlot.Series.ColumnItem(ys1[i], i));
+                seriesB.Items.Add(new OxyPlot.Series.ColumnItem(ys2[i], i));
+            }
+
+            // create a model and add the bars into it
+            var model = new OxyPlot.PlotModel
+            {
+                Title = "Bar Graph (Column Series)"
+            };
+            model.Axes.Add(new OxyPlot.Axes.CategoryAxis());
+            model.Series.Add(seriesA);
+            model.Series.Add(seriesB);
+
+            // load the model into the user control
+            plotView1.Model = model;
+        }
+
+        private void ScatterButton_Click(object sender, EventArgs e)
+        {
+            // generate some random XY data
+            int pointCount = 1_000;
+            double[] xs1 = RandomWalk(pointCount);
+            double[] ys1 = RandomWalk(pointCount);
+            double[] xs2 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // create lines and fill them with data points
+            var line1 = new OxyPlot.Series.LineSeries()
+            {
+                Title = $"Series 1",
+                Color = OxyPlot.OxyColors.Blue,
+                StrokeThickness = 1,
+                MarkerSize = 2,
+                MarkerType = OxyPlot.MarkerType.Circle
+            };
+
+            var line2 = new OxyPlot.Series.LineSeries()
+            {
+                Title = $"Series 2",
+                Color = OxyPlot.OxyColors.Red,
+                StrokeThickness = 1,
+                MarkerSize = 2,
+                MarkerType = OxyPlot.MarkerType.Circle
+            };
+
+            for (int i = 0; i < pointCount; i++)
+            {
+                line1.Points.Add(new OxyPlot.DataPoint(xs1[i], ys1[i]));
+                line2.Points.Add(new OxyPlot.DataPoint(xs2[i], ys2[i]));
+            }
+
+            // create the model and add the lines to it
+            var model = new OxyPlot.PlotModel
+            {
+                Title = $"Scatter Plot ({pointCount:N0} points each)"
+            };
+            model.Series.Add(line1);
+            model.Series.Add(line2);
+
+            // load the model into the user control
+            plotView1.Model = model;
+        }
+
+        private void LineButton_Click(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 1000_000;
+            double[] xs = Consecutive(pointCount);
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // create lines and fill them with data points
+            var line1 = new OxyPlot.Series.LineSeries()
+            {
+                Title = $"Series 1",
+                Color = OxyPlot.OxyColors.Blue,
+                StrokeThickness = 1,
+            };
+
+            var line2 = new OxyPlot.Series.LineSeries()
+            {
+                Title = $"Series 2",
+                Color = OxyPlot.OxyColors.Red,
+                StrokeThickness = 1,
+            };
+
+            for (int i = 0; i < pointCount; i++)
+            {
+                line1.Points.Add(new OxyPlot.DataPoint(xs[i], ys1[i]));
+                line2.Points.Add(new OxyPlot.DataPoint(xs[i], ys2[i]));
+            }
+
+            // create the model and add the lines to it
+            var model = new OxyPlot.PlotModel
+            {
+                Title = $"Line Plot ({pointCount:N0} points each)"
+            };
+            model.Series.Add(line1);
+            model.Series.Add(line2);
+
+            // load the model into the user control
+            plotView1.Model = model;
+        }
+    }
+}
