61c517fbc7c9586344289ea131f28ef78b53e8b7
Scott W Harden <swharden@gmail.com>
2020-05-17 23:54:33 -0400

    livecharts quickstart

---
+++ examples/plotting/livecharts/LiveChartsQuickstart/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace LiveChartsQuickstart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.BarButton = new System.Windows.Forms.Button();
+            this.ScatterButton = new System.Windows.Forms.Button();
+            this.LineButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cartesianChart1.BackColor = System.Drawing.Color.White;
+            this.cartesianChart1.Location = new System.Drawing.Point(12, 41);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(776, 397);
+            this.cartesianChart1.TabIndex = 0;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // BarButton
+            // 
+            this.BarButton.Location = new System.Drawing.Point(12, 12);
+            this.BarButton.Name = "BarButton";
+            this.BarButton.Size = new System.Drawing.Size(75, 23);
+            this.BarButton.TabIndex = 1;
+            this.BarButton.Text = "Bar Graph";
+            this.BarButton.UseVisualStyleBackColor = true;
+            this.BarButton.Click += new System.EventHandler(this.BarButton_Click);
+            // 
+            // ScatterButton
+            // 
+            this.ScatterButton.Location = new System.Drawing.Point(93, 12);
+            this.ScatterButton.Name = "ScatterButton";
+            this.ScatterButton.Size = new System.Drawing.Size(75, 23);
+            this.ScatterButton.TabIndex = 2;
+            this.ScatterButton.Text = "Scatter Plot";
+            this.ScatterButton.UseVisualStyleBackColor = true;
+            this.ScatterButton.Click += new System.EventHandler(this.ScatterButton_Click);
+            // 
+            // LineButton
+            // 
+            this.LineButton.Location = new System.Drawing.Point(174, 12);
+            this.LineButton.Name = "LineButton";
+            this.LineButton.Size = new System.Drawing.Size(75, 23);
+            this.LineButton.TabIndex = 3;
+            this.LineButton.Text = "Line Plot";
+            this.LineButton.UseVisualStyleBackColor = true;
+            this.LineButton.Click += new System.EventHandler(this.LineButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LineButton);
+            this.Controls.Add(this.ScatterButton);
+            this.Controls.Add(this.BarButton);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Button BarButton;
+        private System.Windows.Forms.Button ScatterButton;
+        private System.Windows.Forms.Button LineButton;
+    }
+}
+
---
+++ examples/plotting/livecharts/LiveChartsQuickstart/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LiveChartsQuickstart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Random rand = new Random(0);
+        private double[] RandomWalk(int pointCount = 5, double firstValue = 100, double multiplier = 50)
+        {
+            // return an array of difting random numbers
+            double[] values = new double[pointCount];
+            values[0] = firstValue;
+            for (int i = 1; i < pointCount; i++)
+                values[i] = values[i - 1] + (rand.NextDouble() - .5) * multiplier;
+            return values;
+        }
+
+        private double[] Consecutive(int pointCount = 5)
+        {
+            // return an array of consecutive numbers starting at 1
+            double[] values = new double[pointCount];
+            for (int i = 0; i < pointCount; i++)
+                values[i] = i + 1;
+            return values;
+        }
+
+        private void BarButton_Click(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 5;
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // create series and populate them with data
+            var series1 = new LiveCharts.Wpf.ColumnSeries
+            {
+                Title = "Group A",
+                Values = new LiveCharts.ChartValues<double>(ys1)
+            };
+
+            var series2 = new LiveCharts.Wpf.ColumnSeries()
+            {
+                Title = "Group B",
+                Values = new LiveCharts.ChartValues<double>(ys2)
+            };
+
+            // display the series in the chart control
+            cartesianChart1.Series.Clear();
+            cartesianChart1.Series.Add(series1);
+            cartesianChart1.Series.Add(series2);
+            //cartesianChart1.Zoom = LiveCharts.ZoomingOptions.Xy;
+        }
+
+        private void ScatterButton_Click(object sender, EventArgs e)
+        {
+            // generate some random XY data
+            int pointCount = 100;
+            double[] xs1 = RandomWalk(pointCount);
+            double[] ys1 = RandomWalk(pointCount);
+            double[] xs2 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // create series and populate them with data
+            var series1 = new LiveCharts.Wpf.ScatterSeries
+            {
+                Title = "Group A",
+                Values = new LiveCharts.ChartValues<LiveCharts.Defaults.ObservablePoint>(),
+                PointGeometry = LiveCharts.Wpf.DefaultGeometries.Circle
+            };
+
+            var series2 = new LiveCharts.Wpf.ScatterSeries()
+            {
+                Title = "Group B",
+                Values = new LiveCharts.ChartValues<LiveCharts.Defaults.ObservablePoint>(),
+                PointGeometry = LiveCharts.Wpf.DefaultGeometries.Circle
+            };
+
+            for (int i = 0; i < pointCount; i++)
+            {
+                series1.Values.Add(new LiveCharts.Defaults.ObservablePoint(xs1[i], ys1[i]));
+                series2.Values.Add(new LiveCharts.Defaults.ObservablePoint(xs2[i], ys2[i]));
+            }
+
+            // display the series in the chart control
+            cartesianChart1.Series.Clear();
+            cartesianChart1.Series.Add(series1);
+            cartesianChart1.Series.Add(series2);
+        }
+
+        private void LineButton_Click(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 200;
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // create series and populate them with data
+            var series1 = new LiveCharts.Wpf.LineSeries()
+            {
+                Title = "Group A",
+                Values = new LiveCharts.ChartValues<double>(ys1),
+            };
+
+            var series2 = new LiveCharts.Wpf.LineSeries()
+            {
+                Title = "Group B",
+                Values = new LiveCharts.ChartValues<double>(ys2),
+            };
+
+            // display the series in the chart control
+            cartesianChart1.Series.Clear();
+            cartesianChart1.Series.Add(series1);
+            cartesianChart1.Series.Add(series2);
+        }
+    }
+}
