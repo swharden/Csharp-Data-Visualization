9fe674eda82bbf7669a875bf426a251405ee064c
Scott W Harden <swharden@gmail.com>
2020-05-18 21:53:25 -0400

    scottplot quickstart

---
+++ examples/plotting/scottplot/ScottPlotQuickstart/ScottPlotQuickstart/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ScottPlotQuickstart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BarButton = new System.Windows.Forms.Button();
+            this.ScatterButton = new System.Windows.Forms.Button();
+            this.LineButton = new System.Windows.Forms.Button();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.SuspendLayout();
+            // 
+            // BarButton
+            // 
+            this.BarButton.Location = new System.Drawing.Point(12, 12);
+            this.BarButton.Name = "BarButton";
+            this.BarButton.Size = new System.Drawing.Size(75, 23);
+            this.BarButton.TabIndex = 0;
+            this.BarButton.Text = "Bar Graph";
+            this.BarButton.UseVisualStyleBackColor = true;
+            this.BarButton.Click += new System.EventHandler(this.BarButton_Click);
+            // 
+            // ScatterButton
+            // 
+            this.ScatterButton.Location = new System.Drawing.Point(93, 12);
+            this.ScatterButton.Name = "ScatterButton";
+            this.ScatterButton.Size = new System.Drawing.Size(75, 23);
+            this.ScatterButton.TabIndex = 1;
+            this.ScatterButton.Text = "Scatter Plot";
+            this.ScatterButton.UseVisualStyleBackColor = true;
+            this.ScatterButton.Click += new System.EventHandler(this.ScatterButton_Click);
+            // 
+            // LineButton
+            // 
+            this.LineButton.Location = new System.Drawing.Point(174, 12);
+            this.LineButton.Name = "LineButton";
+            this.LineButton.Size = new System.Drawing.Size(75, 23);
+            this.LineButton.TabIndex = 2;
+            this.LineButton.Text = "Line Plot";
+            this.LineButton.UseVisualStyleBackColor = true;
+            this.LineButton.Click += new System.EventHandler(this.LineButton_Click);
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.formsPlot1.Location = new System.Drawing.Point(12, 41);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(584, 297);
+            this.formsPlot1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 350);
+            this.Controls.Add(this.formsPlot1);
+            this.Controls.Add(this.LineButton);
+            this.Controls.Add(this.ScatterButton);
+            this.Controls.Add(this.BarButton);
+            this.Name = "Form1";
+            this.Text = "ScottPlot Quickstart - C# Data Visualization";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BarButton;
+        private System.Windows.Forms.Button ScatterButton;
+        private System.Windows.Forms.Button LineButton;
+        private ScottPlot.FormsPlot formsPlot1;
+    }
+}
+
---
+++ examples/plotting/scottplot/ScottPlotQuickstart/ScottPlotQuickstart/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlotQuickstart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rand = new Random(0);
+
+        private void BarButton_Click(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 5;
+            double[] ys1 = ScottPlot.DataGen.RandomWalk(rand, pointCount, mult: 50, offset: 100);
+            double[] ys2 = ScottPlot.DataGen.RandomWalk(rand, pointCount, mult: 50, offset: 100);
+
+            // collect the data into groups
+            string[] groupLabels = { "One", "Two", "Three", "Four", "Five" };
+            string[] seriesLabels = { "Group A", "Group B" };
+            double[][] barData = { ys1, ys2 };
+
+            // plot the data
+            formsPlot1.Reset();
+            formsPlot1.plt.PlotBarGroups(groupLabels, seriesLabels, barData);
+
+            // additional styling
+            formsPlot1.plt.Legend(location: ScottPlot.legendLocation.upperLeft);
+            formsPlot1.plt.Axis(y1: 0);
+            formsPlot1.Render();
+        }
+
+        private void ScatterButton_Click(object sender, EventArgs e)
+        {
+            // generate some random X/Y data
+            int pointCount = 500;
+            double[] xs1 = ScottPlot.DataGen.RandomWalk(rand, pointCount);
+            double[] ys1 = ScottPlot.DataGen.RandomWalk(rand, pointCount);
+            double[] xs2 = ScottPlot.DataGen.RandomWalk(rand, pointCount);
+            double[] ys2 = ScottPlot.DataGen.RandomWalk(rand, pointCount);
+
+            // plot the data
+            formsPlot1.Reset();
+            formsPlot1.plt.PlotScatter(xs1, ys1);
+            formsPlot1.plt.PlotScatter(xs2, ys2);
+
+            // additional styling
+            formsPlot1.Render();
+        }
+
+        private void LineButton_Click(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 10_000;
+            double[] ys1 = ScottPlot.DataGen.RandomWalk(rand, pointCount);
+            double[] ys2 = ScottPlot.DataGen.RandomWalk(rand, pointCount);
+
+            // plot the data
+            formsPlot1.Reset();
+            formsPlot1.plt.PlotSignal(ys1);
+            formsPlot1.plt.PlotSignal(ys2);
+
+            // additional styling
+            formsPlot1.plt.Legend();
+            formsPlot1.Render();
+        }
+    }
+}
