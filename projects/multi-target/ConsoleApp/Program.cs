53fb36e04d33502ae4e5112a81bb40a43ca398eb
Scott W Harden <swharden@gmail.com>
2022-01-09 13:29:36 -0500

    project: multi-target (Console)

---
+++ projects/multi-target/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Graphics.Skia;
+
+namespace ConsoleApp;
+
+public static class Program
+{
+    public static void Main()
+    {
+        RenderStandardModel();
+    }
+
+    private static void RenderStandardModel()
+    {
+        using SkiaBitmapExportContext context = new(400, 300, 1.0f);
+
+        Random rand = new();
+        StandardGraphics.Render.TestImage(rand, context.Canvas, context.Width, context.Height);
+
+        using FileStream fs = new("test.png", FileMode.Create);
+        context.WriteToStream(fs);
+    }
+}
--- projects/multi-target/readme.md
+++ projects/multi-target/readme.md
@@ -4,6 +4,7 @@ This project demonstrates how to create multi-targeted app using:
 * **.NET Standard 2.0** library for all drawing logic
   * Drawing performed by `Microsoft.Maui.Graphics`
   * No dependency on `System.Drawing`
+* **Console Application** renders using `Microsoft.Mui.Graphics.Skia`
 * **Windows Forms** apps for rendering using Skia + OpenGL
   * .NET Framework 4.8
   * .NET 6.0
