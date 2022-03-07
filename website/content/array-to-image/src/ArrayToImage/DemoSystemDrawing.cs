1117cdefe801ddbc94c30634c01ed3da10856a8d
Scott W Harden <swharden@gmail.com>
2022-03-06 22:27:38 -0500

    content: start Array to image project

---
+++ website/content/array-to-image/src/ArrayToImage/DemoSystemDrawing.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace ArrayToImage;
+
+public static class DemoSystemDrawing
+{
+    public static void SaveImageRgb(string filePath, byte[,,] pixelArray)
+    {
+        filePath = System.IO.Path.GetFullPath(filePath);
+        using Bitmap bmp = GetBitmap(pixelArray);
+        bmp.Save(filePath);
+        Console.WriteLine(filePath);
+    }
+
+    private static Bitmap GetBitmap(byte[,,] pixelArray)
+    {
+        int width = pixelArray.GetLength(1);
+        int height = pixelArray.GetLength(0);
+        int stride = (width % 4 == 0) ? width : width + 4 - width % 4;
+        int bytesPerPixel = 3;
+
+        byte[] bytes = new byte[stride * height * bytesPerPixel];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int offset = (y * stride + x) * bytesPerPixel;
+                bytes[offset + 0] = pixelArray[y, x, 2]; // blue
+                bytes[offset + 1] = pixelArray[y, x, 1]; // green
+                bytes[offset + 2] = pixelArray[y, x, 0]; // red
+            }
+        }
+
+        PixelFormat formatOutput = PixelFormat.Format24bppRgb;
+        Rectangle rect = new(0, 0, width, height);
+        Bitmap bmp = new(stride, height, formatOutput);
+        BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadOnly, formatOutput);
+        Marshal.Copy(bytes, 0, bmpData.Scan0, bytes.Length);
+        bmp.UnlockBits(bmpData);
+
+        Bitmap bmp2 = new(width, height, PixelFormat.Format32bppPArgb);
+        Graphics gfx2 = Graphics.FromImage(bmp2);
+        gfx2.DrawImage(bmp, 0, 0);
+
+        return bmp2;
+    }
+}
---
+++ website/content/array-to-image/src/ArrayToImage/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ArrayToImage;
+
+public static class Program
+{
+    public static void Main()
+    {
+        byte[,,] pixelArray = GetTestPattern();
+        DemoSystemDrawing.SaveImageRgb("rgb-SystemDrawing.png", pixelArray);
+    }
+
+    private static byte[,,] GetTestPattern(int width = 653, int height = 487, int channels = 3)
+    {
+        byte[,,] pixelArray = new byte[height, width, channels];
+
+        double xPeriod = 20;
+        double yPeriod = 20;
+
+        for (int y = 0; y < pixelArray.GetLength(0); y++)
+        {
+            for (int x = 0; x < pixelArray.GetLength(1); x++)
+            {
+                double xSin = (Math.Sin(x / xPeriod) + 1) / 2;
+                double ySin = (Math.Sin(y / yPeriod) + 1) / 2;
+                double vSin = (xSin + ySin) * 128;
+
+                pixelArray[y, x, 2] = (byte)vSin; // blue
+                pixelArray[y, x, 1] = (byte)x; // green
+                pixelArray[y, x, 0] = (byte)y; // red
+            }
+        }
+
+        return pixelArray;
+    }
+}
