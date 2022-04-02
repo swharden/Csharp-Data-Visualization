b35ae82b825d5a0af16ead256d0e09910762b1f4
Scott W Harden <swharden@gmail.com>
2022-04-02 19:50:10 -0400

    project: SkiaSharp video

---
+++ projects/skiasharp/video/GraphicsToVideo/Program.cs
@@ -0,0 +1,46 @@
+﻿using FFMpegCore;
+using FFMpegCore.Pipes;
+using SkiaSharp;
+
+namespace GraphicsToVideo;
+
+public static class Program
+{
+    public static void Main()
+    {
+        string outputPath = Path.GetFullPath("output.webm");
+        if (File.Exists(outputPath))
+            File.Delete(outputPath);
+
+        var frames = CreateFrames(count: 150, width: 400, height: 300);
+        var videoFramesSource = new RawVideoPipeSource(frames) { FrameRate = 30 };
+        var success = FFMpegArguments
+            .FromPipeInput(videoFramesSource)
+            .OutputToFile("output.webm", overwrite: true, options => options.WithVideoCodec("libvpx-vp9"))
+            .ProcessSynchronously();
+
+        Console.WriteLine($"Saved: {outputPath}");
+    }
+
+    static IEnumerable<IVideoFrame> CreateFrames(int count, int width, int height)
+    {
+        using SKFont textFont = new(SKTypeface.FromFamilyName("consolas"), size: 32);
+        using SKPaint textPaint = new(textFont) { Color = SKColors.Yellow, TextAlign = SKTextAlign.Center };
+        using SKPaint rectanglePaint = new() { Color = SKColors.Green, Style = SKPaintStyle.Fill };
+        SKColor backgroundColor = SKColors.Navy;
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine($"Rendering frame {i + 1} of {count}");
+            using SKBitmap bmp = new(width, height);
+            using SKCanvas canvas = new(bmp);
+            canvas.Clear(backgroundColor);
+            canvas.DrawRect(i, i, i * 2, i * 2, rectanglePaint);
+            canvas.DrawText("SkiaSharp", bmp.Width / 2, bmp.Height * .4f, textPaint);
+            canvas.DrawText($"Frame {i}", bmp.Width / 2, bmp.Height * .6f, textPaint);
+
+            using SKBitmapFrame frame = new(bmp);
+            yield return frame;
+        }
+    }
+}
---
+++ projects/skiasharp/video/GraphicsToVideo/SKBitmapFrame.cs
@@ -0,0 +1,36 @@
+﻿using FFMpegCore.Pipes;
+using SkiaSharp;
+
+namespace GraphicsToVideo;
+
+internal class SKBitmapFrame : IVideoFrame, IDisposable
+{
+    public int Width => Source.Width;
+    public int Height => Source.Height;
+    public string Format => "bgra";
+
+    private readonly SKBitmap Source;
+
+    public SKBitmapFrame(SKBitmap bmp)
+    {
+        if (bmp.ColorType != SKColorType.Bgra8888)
+            throw new NotImplementedException("only 'bgra' color type is supported");
+
+        Source = bmp;
+    }
+
+    public void Serialize(Stream stream)
+    {
+        stream.Write(Source.Bytes);
+    }
+
+    public async Task SerializeAsync(Stream stream, CancellationToken token)
+    {
+        await stream.WriteAsync(Source.Bytes, token).ConfigureAwait(false);
+    }
+
+    public void Dispose()
+    {
+        Source.Dispose();
+    }
+}
